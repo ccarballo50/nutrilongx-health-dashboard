74886c026a800637820f029318eff2f6d2329b81
ccarballo50 <58879757+ccarballo50@users.noreply.github.com>
2025-08-12 14:22:43 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "../App"; // ajusta la ruta si tu index.tsx está en /src
+import App from "./App"; // 👈 importante: ./App
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
