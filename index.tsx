bf68783b68c155fdaafb6adde906a2570eb6fae0
ccarballo50 <58879757+ccarballo50@users.noreply.github.com>
2025-08-12 23:29:38 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App"; // 👈 importante: ./App
+import App from "./App"; // 👈 OBLIGATORIO: ./App (no ../App)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
