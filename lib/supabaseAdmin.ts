06ddc2409d907c246218352383376cd4e077de09
ccarballo50 <58879757+ccarballo50@users.noreply.github.com>
2025-08-13 00:04:04 +0200

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export function supabaseAdmin() {
-  const url = process.env.https://iabapdubybicoegeswvq.supabase.co!;
-  const key = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmFwZHVieWJpY29lZ2Vzd3ZxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDk4NDQwMCwiZXhwIjoyMDcwNTYwNDAwfQ.aO7WylE1VieZVhSpmf91FGuXlPOaGVeVm6zvVHuWLpI!;
+  const url = process.env.SUPABASE_URL!;
+  const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
   return createClient(url, key, { global: { fetch } });
 }
