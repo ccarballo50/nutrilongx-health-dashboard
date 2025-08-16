08bcb6beebdb10eb87208083739b0c27487ee85e
ccarballo50 <58879757+ccarballo50@users.noreply.github.com>
2025-08-16 09:50:03 +0200

    Update Notifications.tsx

--- pages/admin/Notifications.tsx
+++ pages/admin/Notifications.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { listSubscriptions, subscribeChannels, unsubscribe } from '../../services/notify';
 import { sendNotification, listAudience } from '@services/notify';
 
 
